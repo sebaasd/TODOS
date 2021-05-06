2db099ee5154f45f6d3727bf558fcff6cb5fa2fe
sebaasd <61253100+sebaasd@users.noreply.github.com>
2021-05-06 00:37:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-//Select DOM
+//dom
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
-//Event Listeners
+//eventos
 document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteTodo);
 filterOption.addEventListener("click", filterTodo);
 
-//Functions
+//funciones
 
 function addTodo(e) {
   //Prevent natural behaviour
